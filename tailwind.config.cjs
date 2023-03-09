439e1906a75c824e7ee6f5aabd8c6c4282165ab5
raushangpt <rk880488@gmail.com>
2023-03-09 23:19:16 +0530

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.svelte'],
-  theme: {
-    colors: {
-      'gray-100': 'hsl(0, 0%, 100%)',
-      'gray-200': 'hsla(0, 0%, 100%, 0.75)',
-      'gray-300': 'hsla(0, 0%, 100%, 0.6)',
-      'blue-100': 'hsl(244, 38%, 16%)',
-      'blue-200': 'hsl(233, 47%, 7%)',
-      'violet': 'hsl(277, 64%, 61%)',
+    content: ['./index.html'],
+    theme: {
+        colors: {
+            'gray-100': 'hsl(0, 0%, 100%)',
+            'gray-200': 'hsla(0, 0%, 100%, 0.75)',
+            'gray-300': 'hsla(0, 0%, 100%, 0.6)',
+            'blue-100': 'hsl(244, 38%, 16%)',
+            'blue-200': 'hsl(233, 47%, 7%)',
+            'violet': 'hsl(277, 64%, 61%)',
+        },
+        fontFamily: {
+            'inter': ['Inter', 'sans-serif'],
+            'lexend-deca': ['Lexend Deca', 'sans-serif'],
+        },
+        extend: {},
     },
-    fontFamily: {
-      'inter': ['Inter', 'sans-serif'],
-      'lexend-deca': ['Lexend Deca', 'sans-serif'],
-    },
-    extend: {},
-  },
-  plugins: [],
+    plugins: [],
 };
